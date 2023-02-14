8fd506e3ba9a50fe19f1b10ae254955b489259e5
effervescent000 <kislio01@gmail.com>
2023-02-13 21:20:35 -0500

    bring in alembic

--- app/main.py
+++ app/main.py
@@ -2,5 +2,9 @@ from fastapi import FastAPI
 
 from .auth import routes as auth_routes
 
+# from .database import Base, engine
+
+# Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 app.include_router(auth_routes.router)
