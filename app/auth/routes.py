840e577fc37bbf2e3e639d23eb2bcc8b1f4cd81d
effervescent000 <kislio01@gmail.com>
2023-02-13 21:26:31 -0500

    let's try auth

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 
@@ -6,17 +7,21 @@ from .models import UserRead, UserWrite
 from ..deps import get_db
 from ..tags import Tags
 from ..schema import User
+from ..utils.http_utils import bad_request
 
-router = APIRouter(prefix="/users", tags=[Tags.users])
+router = APIRouter(prefix="/auth", tags=[Tags.users])
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
 
 @router.get("/")
-async def get_users(db: Session = Depends(get_db)) -> list[UserRead]:
+async def get_users(
+    db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)
+) -> list[UserRead]:
     stmnt = select(User)
     return [UserRead.from_orm(user) for user in db.scalars(stmnt)]
 
 
-@router.post("/new")
+@router.post("/")
 async def add_user(
     user: UserWrite,
     db: Session = Depends(get_db),
@@ -25,3 +30,19 @@ async def add_user(
     db.add(out)
     db.commit()
     return UserRead.from_orm(out)
+
+
+@router.post("/token")
+async def login(
+    db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()
+):
+    user = db.scalars(
+        select(User).where(User.email == form_data.username)
+    ).one_or_none()
+    if not user:
+        raise bad_request(message="Incorrect username or password")
+    user = UserWrite(**user.dict())
+    hashed_password = form_data.password
+    if hashed_password != user.hashed_password:
+        raise bad_request(message="Incorrect username or password")
+    return {"access_token": user.username, "token_type": "bearer"}
