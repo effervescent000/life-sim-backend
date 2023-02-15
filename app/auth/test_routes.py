8d407368fa4afeb423f13fc72b9e6c3cc086afe6
effervescent000 <kislio01@gmail.com>
2023-02-15 14:11:35 -0500

    tests for invalid logins

--- app/auth/test_routes.py
+++ app/auth/test_routes.py
@@ -26,6 +26,20 @@ def test_post_user(client: TestClient):
     }
 
 
-def test_login(client: TestClient):
+def test_login_valid(client: TestClient):
     response = client.post("/auth/login/", json=world.user_factory())
     assert response.json() == {"access_token": "some person", "token_type": "bearer"}
+
+
+def test_login_invalid_email(client: TestClient):
+    response = client.post(
+        "/auth/login/", json=world.user_factory(email="wrong@email.com")
+    )
+    assert response.json() == {"detail": "Incorrect username or password"}
+
+
+def test_login_invalid_password(client: TestClient):
+    response = client.post(
+        "/auth/login/", json=world.user_factory(password="wrong password")
+    )
+    assert response.json() == {"detail": "Incorrect username or password"}
