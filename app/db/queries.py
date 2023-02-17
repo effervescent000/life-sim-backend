183dbff4c1af933886abc083273a9d2147602ff9
effervescent000 <kislio01@gmail.com>
2023-02-16 23:33:58 -0500

    upsert mirror models func

--- app/db/queries.py
+++ app/db/queries.py
@@ -14,3 +14,16 @@ def select_mirror_models(
     if model.use_scalars:
         records = records.scalars()
     return [model.from_orm(record) for record in records.all()]
+
+
+def upsert_mirror_models(
+    db: Session, read_model: Type[TMirrorModel], write_models: list[TMirrorModel]
+) -> list[TMirrorModel]:
+    mutations: list[TMirrorModel] = []
+
+    for model in write_models:
+        mutated = db.merge(model.to_orm())
+        db.commit()
+        mutations.append(read_model.from_orm(mutated))
+
+    return mutations
